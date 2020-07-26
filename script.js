f8c732a8387d183e73b3029cf16e65894cf522ba
Jake Haberle <jake.haberle@gmail.com>
2020-07-26 13:31:04 -0600

    added variables

---
+++ script.js
@@ -0,0 +1,25 @@
+let timeNow = moment().format('dddd, MMMM Do YYYY');
+let hourNow = moment().format('h');
+let hourNow24= parseInt(moment().format('HH'));
+let hourNowInt = parseInt(hourNow);
+let Timer;
+
+const hour1 = $('#time1').text();
+const hour2 = $('#time2').text();
+const hour3 = $('#time3').text();
+const hour4 = $('#time4').text();
+const hour5 = $('#time5').text();
+const hour6 = $('#time6').text();
+const hour7 = $('#time7').text();
+const hour8 = $('#time8').text();
+const hour9 = $('#time9').text();
+
+const hour1Int = parseInt(hour1);
+const hour2Int = parseInt(hour2);
+const hour3Int = parseInt(hour3);
+const hour4Int = parseInt(hour4);
+const hour5Int = parseInt(hour5);
+const hour6Int = parseInt(hour6);
+const hour7Int = parseInt(hour7);
+const hour8Int = parseInt(hour8);
+const hour9Int = parseInt(hour9);
